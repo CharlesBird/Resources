ebb19d8a7641f2313c356fdf627eddd319b78e61
CharlesBird <1016784928@qq.com>
2017-11-07 17:39:39 +0800

    机器学习实战  决策树

---
+++ coding/machinelearn/trees.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+from math import log
+import operator
+
+def calcShannonEnt(dataSet):
+    """计算熵方法"""
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+
+def createDataSet():
+    dataSet = [
+        [1, 1, 'yes'],
+        [1, 1, 'yes'],
+        [1, 0, 'no'],
+        [0, 1, 'no'],
+        [0, 1, 'no']
+    ]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+# 根据数据集计算熵
+# myDat, labels = createDataSet()
+# print myDat
+# print calcShannonEnt(myDat)
+# myDat[0][-1] = 'maybe'
+# print calcShannonEnt(myDat)
+
+
+def splitDataSet(dataSet, axis, value):
+    """划分数据集"""
+    reDataset = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            reDataset.append(reducedFeatVec)
+    return reDataset
+
+# 数据集划分
+# myDat, labels = createDataSet()
+# print myDat
+# print splitDataSet(myDat, 0, 1)
+# print splitDataSet(myDat, 0, 0)
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+# 选择最好的数据集划分方式
+# myDat, labels = createDataSet()
+# print chooseBestFeatureToSplit(myDat)
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    """递归方式"""
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+# 构造树
+myDat, labels = createDataSet()
+print createTree(myDat, labels)
