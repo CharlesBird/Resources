b771d7286932d632403baa8a5ecbae33b9e7487d
CharlesBird <1016784928@qq.com>
2017-11-08 23:08:31 +0800

    delete

--- odoo/linyan_addons/linyan_contract/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import linyan_contract
-import wizard
-import report
--- odoo/linyan_addons/linyan_contract/report/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-import linyan_cash_report
-import linyan_profit_report
-import linyan_cash_avg_price_report
