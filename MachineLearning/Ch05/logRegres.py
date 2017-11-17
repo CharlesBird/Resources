ed129d4652b914f7240e3d5679c9b0163900718a
CharlesBird <1016784928@qq.com>
2017-11-17 17:33:31 +0800

    logistic

---
+++ MachineLearning/Ch05/logRegres.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from numpy import *
+import matplotlib.pyplot as plt
+
+def loadDataSet():
+    dataMat = []
+    labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split("\t")
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
+
+
+def sigmoid(inX):
+    return 1.0/(1+exp(-inX))
+
+
+def gradAscent(dataMatIn, classLabels):
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    m, n = shape(dataMatrix)
+    alpha = 0.001
+    maxCycles = 500
+    weights = ones((n, 1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix*weights)
+        error = labelMat-h
+        weights = weights + alpha * dataMatrix.transpose() * error
+    return weights
+
+# dataArr, labelMat = loadDataSet()
+# print(gradAscent(dataArr, labelMat))
+
+
+def plotBestFit(wei):
+    weights = wei.getA()
+    dataMat, labelMat = loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []
+    xcord2 = []
+    ycord1 = []
+    ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1])
+            ycord1.append(dataArr[i, 2])
+        else:
+            xcord2.append(dataArr[i, 1])
+            ycord2.append(dataArr[1, 2])
