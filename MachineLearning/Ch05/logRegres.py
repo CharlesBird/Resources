3a31057943dee34c998ddea504b25d6b81577d4e
CharlesBird <1016784928@qq.com>
2017-11-20 17:35:13 +0800

    logistic回归

--- MachineLearning/Ch05/logRegres.py
+++ MachineLearning/Ch05/logRegres.py
@@ -49,4 +49,18 @@ def plotBestFit(wei):
             ycord1.append(dataArr[i, 2])
         else:
             xcord2.append(dataArr[i, 1])
-            ycord2.append(dataArr[1, 2])
+            ycord2.append(dataArr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - weights[1] * x)/weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
+
+dataArr, labelMat = loadDataSet()
+plotBestFit(gradAscent(dataArr, labelMat))
