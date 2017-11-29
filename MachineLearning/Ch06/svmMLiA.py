3344bb5d78092be219c2f7f5b9c799f0249e945d
CharlesBird <1016784928@qq.com>
2017-11-29 17:39:45 +0800

    SVM

--- MachineLearning/Ch06/svmMLiA.py
+++ MachineLearning/Ch06/svmMLiA.py
@@ -1,3 +1,53 @@
 # -*- coding: utf-8 -*-
 from numpy import *
 import matplotlib.pyplot as plt
+
+def loadDataSet(filename):
+    dataMat = []
+    labelMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr = line.strip().split('\t')
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    return dataMat, labelMat
+
+
+def selectJrand(i, m):
+    j = i
+    while j==i:
+        j = int(random.uniform(0, m))
+    return j
+
+
+def clipAlpha(aj, H, L):
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
+
+# dataArr, labelArr = loadDataSet('testSet.txt')
+# print(dataArr, labelArr)
+
+
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    b = 0
+    m, n = shape(dataMatrix)
+    alphas = mat(zeros((m, 1)))
+    iter = 0
+    while iter < maxIter:
+        alphaPairsChanged = 0
+        for i in range(m):
+            fXi = float(mutiply(alphas, labelMat).T*(dataMatrix*dataMatrix[i, :].T)) + b
+            Ei = fXi - float(labelMat[i])
+            if ((labelMat[i]*Ei < -toler) and (alphas[i] < C)) or ((labelMat[i]*Ei > toter) and (alphas[i] > 0)):
+                j = selectJrand(i, m)
+                fXj = float(multiply(alphas, labelMata).T*(dataMatrix*dataMatrix[j, :].T)) + b
+                Ej = fXj - float(labelMat[j])
+                alphaIold = alphas[i].copy()
+                alphaJold = alphas[j].copy()
+                if (labelMat[i]) != labelMat[j]:
+                    L = max()
