4a61a2c1f7024fb73b441d65b4230e170c6b9caa
CharlesBird <1016784928@qq.com>
2018-03-08 14:19:26 +0800

    注释

--- coding/test_capture.py
+++ coding/test_capture.py
@@ -2,6 +2,6 @@ from PIL import ImageGrab
 
 im = ImageGrab.grab((276, 485, 343, 500))  # 截图系统时间
 
-im = ImageGrab.grab((304, 500, 348, 515))  # 截图最低成交价
+# im = ImageGrab.grab((304, 500, 348, 515))  # 截图最低成交价
 
 im.save('D:\github\coding\hha.jpg')
