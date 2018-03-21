f1b6e1f92b7025789c288b6a2260172e99daf684
Charles <zhanghuaicheng@unovo.com.cn>
2018-03-21 17:37:29 +0800

    python链表实现

--- coding/py_datastructure/linkedlist.py
+++ coding/py_datastructure/linkedlist.py
@@ -12,16 +12,26 @@ class LinkedList(object):
 
     def __init__(self):
         self.head = None
-        self.lenth = 0
+
+    def size(self):
+        """链表大小"""
+        count = 0
+        if self.head is not None:
+            current_node = self.head
+            while current_node is not None:
+                count += 1
+                current_node = current_node.pnext
+        return count
 
     def is_empty(self):
-        return self.lenth
+        """链表是否为空"""
+        if self.head is None or self.size() == 0:
+            return True
+        return False
 
-    def append(self, n):
-        if isinstance(n, Node):
-            raise Exception("This not a Node")
-        else:
-            this_node = Node(n)
+    def append(self, data):
+        """给链表添加元素，默认添加在尾部"""
+        this_node = Node(data)
         if self.is_empty():
             self.head = this_node
         else:
@@ -29,4 +39,145 @@ class LinkedList(object):
             while node.pnext:
                 node = node.pnext
             node.pnext = this_node
-        self.lenth += 1
+        return self.head
+
+    def prepend(self, data):
+        """给链表头部添加元素"""
+        new_head = Node(data)
+        if self.is_empty():
+            self.head = new_head
+        else:
+            new_head.pnext = self.head
+            self.head = new_head
+        return self.head
+
+    def insert(self, index, data):
+        """列表的插入"""
+        if not isinstance(index, int):
+            raise Exception("Index must be Integer")
+        if self.is_empty():
+            raise Exception("It's a empty list")
+        if index <= 0 or index > self.size():
+            raise Exception("Index value is out of range")
+        else:
+            if index == 1:
+                self.prepend(data)
+            else:
+                this_node = Node(data)
+                pre_node = self.head
+                current_node = self.head.next
+                while index > 2:
+                    pre_node = current_node
+                    current_node = pre_node.pnext
+                    index -= 1
+                pre_node.pnext = this_node
+                this_node.pnext = current_node
+        return None
+
+    def delete(self, index):
+        """链表指定位置删除"""
+        if not isinstance(index, int):
+            raise Exception("Index must be Integer")
+        if index <= 0 or index > self.size():
+            raise Exception("Index value is out of range")
+        elif index == 1:
+            self.head = self.head.pnext
+        else:
+            current_node = self.head
+            while index > 1:
+                current_node = current_node.pnext
+                index -= 1
+            current_node.pnext = current_node.pnext.pnext
+        return None
+
+    def update(self, index, data):
+        """链表指定位置更新"""
+        if not isinstance(index, int):
+            raise Exception("Index must be Integer")
+        if index <= 0 or index > self.size():
+            raise Exception("Index value is out of range")
+        elif index == 1:
+            this_node = Node(data)
+            this_node.pnext = self.head.pnext
+            self.head = this_node
+        else:
+            current_node = self.head
+            while index > 1:
+                current_node = current_node.pnext
+                index -= 1
+            current_node.data = data
+        return None
+
+    def get_value(self, index):
+        """指定位置获取列表的值"""
+        if not isinstance(index, int):
+            raise Exception("Index must be Integer")
+        if index <= 0 or index > self.size():
+            raise Exception("Index value is out of range")
+        if index == 1:
+            return self.head.data
+        else:
+            current_node = self.head
+            while index > 1:
+                current_node = current_node.pnext
+                index -= 1
+            return current_node.data
+
+    def peek(self):
+        """获取列表尾部值，不删除节点"""
+        return self.get_value(self.size())
+
+    def pop(self):
+        """获取列表尾部值，并删除节点"""
+        if self.head is None or self.size() == 0:
+            raise Exception("List is Null")
+        if self.size() == 1:
+            top = self.head.data
+            self.head = None
+            return top
+        else:
+            pre_node = self.head
+            current_node = pre_node.pnext
+            while current_node.pnext is not None:
+                pre_node = current_node
+                current_node = current_node.pnext
+            top = current_node.data
+            current_node = None
+            pre_node.pnext = None
+            return top
+
+    def clear(self):
+        """清除所有节点"""
+        self.head = None
+        return self.head
+
+    def reverse(self):
+        """单链表逆序"""
+        if self.head is None or self.size() == 0:
+            return None
+        if self.size() == 1:
+            return None
+        else:
+            pre = None
+            current_node = self.head
+            while current_node is not None:
+                tmp = current_node.pnext
+                current_node.pnext = pre
+                pre = current_node
+                current_node = tmp
+            self.head = pre
+        return None
+
+    def print_linked_list(self):
+        """打印所有节点值"""
+        if self.is_empty():
+            return None
+        else:
+            current_node = self.head
+            result = 'head --> %s' % current_node.data
+            while current_node.pnext is not None:
+                current_node = current_node.pnext
+                result += ' --> %s' % current_node.data
+            return result
+
+
