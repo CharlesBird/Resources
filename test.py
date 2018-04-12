79268f1bbbec9e0ba2372c8145a176b95dda737e
Charles <zhanghuaicheng@unovo.com.cn>
2018-04-12 10:14:48 +0800

    Signed-off-by: Charles <zhanghuaicheng@unovo.com.cn>

--- test.py
+++ test.py
@@ -1848,8 +1848,8 @@ url, db, username, password = 'http://192.168.3.51:8069', 'db-lianyuplus', 'supp
 common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
 uid = common.authenticate(db, username, password, {})
 models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url), allow_none=True)
-# models.execute_kw(db, uid, password, 'unovo.works', 'create_next_works', ["days"])
-models.execute_kw(db, uid, password, 'unovo.works', 'create_next_works', ["weeks"])
+models.execute_kw(db, uid, password, 'unovo.works', 'create_next_works', ["days"])
+# models.execute_kw(db, uid, password, 'unovo.works', 'create_next_works', ["weeks"])
 # employee = models.execute_kw(db, uid, password, 'hr.employee', 'search_read', [[], ['user_id']])
 # for em in employee:
 #     user_id = em['user_id'][0]
