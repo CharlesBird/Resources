ff21de26ed9c92df2f18c256cb9b9c75fce3350a
CharlesBird <1016784928@qq.com>
2018-05-17 00:23:14 +0800

    决策边界

--- MachineLearning/Jupyter/PlayML/kNN.py
+++ MachineLearning/Jupyter/PlayML/kNN.py
@@ -5,6 +5,8 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler, minmax_scale
 from sklearn.linear_model import SGDRegressor
+from matplotlib.colors import ListedColormap
+from matplotlib import contour
 
 
 class KNNClassifier:
