d89aef30366a07c17f15ee378e0e884d8b6768f8
CharlesBird <1016784928@qq.com>
2019-03-03 15:17:25 +0800

    es

--- coding/Elasticsearch/test.py
+++ coding/Elasticsearch/test.py
@@ -1,46 +1,46 @@
 from pymongo import MongoClient
 from elasticsearch import Elasticsearch
 
-_db = MongoClient('mongodb://127.0.0.1:27017')['blog']
-print(_db)
-_es = Elasticsearch()
+# _db = MongoClient('mongodb://127.0.0.1:27017')['blog']
+# print(_db)
+_es = Elasticsearch(['118.190.149.30'])
 print(_es)
 
-_index_mappings = {
-    "mappings": {
-        "user": {
-            "properties": {
-                "title": {"type": "text"},
-                "name": {"type": "text"},
-                "age": {"type": "integer"}
-            }
-        },
-        "blogpost": {
-            "properties": {
-                "title": {"type": "text"},
-                "body": {"type": "text"},
-                "user_id": {
-                    "type": "keyword"
-                },
-                "created": {
-                    "type": "date"
-                }
-            }
-        }
-    }
-}
-
-_es.index(index="my_index", doc_type="test_type", id=1, body={"name": "python", "addr": "上海"})
-
-_es.create(index="my_index", doc_type="test_type", id=1, ignore=409, body={"name": "python", "addr": "上海"})
-
+# _index_mappings = {
+#     "mappings": {
+#         "user": {
+#             "properties": {
+#                 "title": {"type": "text"},
+#                 "name": {"type": "text"},
+#                 "age": {"type": "integer"}
+#             }
+#         },
+#         "blogpost": {
+#             "properties": {
+#                 "title": {"type": "text"},
+#                 "body": {"type": "text"},
+#                 "user_id": {
+#                     "type": "keyword"
+#                 },
+#                 "created": {
+#                     "type": "date"
+#                 }
+#             }
+#         }
+#     }
+# }
+#
+# _es.index(index="my_index", doc_type="test_type", id=1, body={"name": "python", "addr": "上海"})
+#
+# _es.create(index="my_index", doc_type="test_type", id=1, ignore=409, body={"name": "python", "addr": "上海"})
+#
 result = _es.search(index="my_index", doc_type="test_type")
 print(result)
-for item in result["hits"]["hits"]:
-    print(item["_source"])
-result = _es.get(index="my_index", doc_type="test_type", id=1)
-print(result)
-result = _es.delete(index="my_index", doc_type="test_type", id=1)
-print(result)
-result = _es.search(index="my_index", doc_type="test_type")
-print(result)
+# for item in result["hits"]["hits"]:
+#     print(item["_source"])
+# result = _es.get(index="my_index", doc_type="test_type", id=1)
+# print(result)
+# result = _es.delete(index="my_index", doc_type="test_type", id=1)
+# print(result)
+# result = _es.search(index="my_index", doc_type="test_type")
+# print(result)
