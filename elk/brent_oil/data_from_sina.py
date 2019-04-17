3d65e7c521b29fe015f0f88e26a830ff1c73a45b
CB <1016784928@qq.com>
2019-04-17 18:04:11 +0800

    sina获取布伦特数据

--- elk/brent_oil/data_from_sina.py
+++ elk/brent_oil/data_from_sina.py
@@ -1,5 +1,40 @@
 import requests
-url = 'http://hq.sinajs.cn/list=hf_OIL,hf_CL,hf_CAD,hf_S'
+import re
+import time
+import asyncio
+import aiohttp
+url = 'http://hq.sinajs.cn/list=hf_OIL'
+check = None
+while True:
+    response = requests.get(url)
+    resp = response.text
 
-response = requests.get(url)
-print(response.text)
+    pattern = re.compile('="(.*)"')
+    s = pattern.findall(resp)
+    res = {}
+    if s:
+        s_l = s[0].split(',')
+        change_time = ' '.join([s_l[12], s_l[6]])
+        if check is None:
+            check = change_time
+        else:
+            if check == change_time:
+                continue
+        res.update({
+            'last_price': float(s_l[0]),
+            'change_value': float(s_l[1]),
+            'buy_price': float(s_l[2]),
+            'sell_price': float(s_l[3]),
+            'max_price': float(s_l[4]),
+            'min_price': float(s_l[5]),
+            'yestoday_price': float(s_l[7]),
+            'open_price': float(s_l[8]),
+            'amount': float(s_l[9]),
+            'buy_amount': float(s_l[10]),
+            'sell_amount': float(s_l[11]),
+            'name': s_l[13],
+            'change_time': change_time
+        })
+        check = change_time
+    time.sleep(1)
+    print(res)
