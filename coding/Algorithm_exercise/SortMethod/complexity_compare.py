bfb0bcdfc9328eb1a7a523cd164fb9d588c1d9e5
CB <1016784928@qq.com>
2019-04-17 17:21:38 +0800

    排序算法比较

--- coding/Algorithm_exercise/SortMethod/complexity_compare.py
+++ coding/Algorithm_exercise/SortMethod/complexity_compare.py
@@ -11,6 +11,7 @@ from quick import quickSort
 from radix import radixSort
 from selection import selectionSort
 from shell import shellSort
+from pythonSort import pythonSort, pythonSorted
 
 def main(method, ran, size):
     arg = [random.randint(1, ran) for _ in range(size)]
@@ -21,9 +22,17 @@ def main(method, ran, size):
 
 
 if __name__ == '__main__':
-    for i in [100, 1000, 10000, 100000]:
-        for j in [100, 1000, 10000]:
-            print('range{}_{}'.format(i, j))
-            for name in ['bubbleSort', 'selectionSort', 'insertionSort', 'shellSort', 'mergeSort', 'quickSort', 'heapSort', 'countingSort', 'bucketSort', 'radixSort']:
+    # for i in [100, 1000, 10000, 100000]:
+    #     for j in [100, 1000, 10000]:
+    #         print('range{}_{}'.format(i, j))
+    #         for name in ['bubbleSort', 'selectionSort', 'insertionSort', 'shellSort', 'mergeSort', 'quickSort', 'heapSort', 'countingSort', 'bucketSort', 'radixSort', 'pythonSort', 'pythonSorted']:
+    #             res = [main(name, i, j), main(name, i, j), main(name, i, j)]
+    #             print('排序方式{}, 数字范围{}, 排序长度{}, 三次耗时: {}, 平均耗时: {}'.format(name, i, j, res, np.mean(res)))
+
+    for name in ['bubbleSort', 'selectionSort', 'insertionSort', 'shellSort', 'mergeSort', 'quickSort', 'heapSort',
+                 'countingSort', 'bucketSort', 'radixSort', 'pythonSort', 'pythonSorted']:
+        for i in [100, 1000, 10000, 100000]:
+            for j in [100, 1000, 10000]:
+                # print('range{}_{}'.format(i, j))
                 res = [main(name, i, j), main(name, i, j), main(name, i, j)]
-                print('排序方式{}, 数字范围{}, 排序长度{}, 三次耗时: {}, 平均耗时: {}'.format(name, i, j, res, np.mean(res)))
+                print('排序方式{}, 数字范围{}, 序列长度{}, 三次耗时: {}, 平均耗时: {}'.format(name, i, j, res, np.mean(res)))
---
+++ coding/Algorithm_exercise/SortMethod/pythonSort.py
@@ -0,0 +1,6 @@
+def pythonSort(myList):
+    myList.sort()
+    return myList
+
+def pythonSorted(myList):
+    return sorted(myList)
