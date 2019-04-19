ac9942f8b0eac108365b70d2738f3c6c41d08a70
CB <1016784928@qq.com>
2019-04-19 13:34:45 +0800

    sina获取布伦特数据

--- coding/generator/test.py
+++ coding/generator/test.py
@@ -1,2 +1,38 @@
 import asyncio
-import threading
+import aiopg
+from queue import Queue
+
+
+async def build_db_pool(loop):
+    try:
+        dns = 'dbname={} user={} password={} host={} port={}'.format('CCCC02216', 'ccerp',
+                                                                     '', '127.0.0.1', 5432)
+        pool = await aiopg.create_pool(dns, loop=loop)
+    except Exception as e:
+        print('Unable to connect.{}'.format(e))
+        pool = None
+    return pool
+
+
+async def write_to_db(pool, loop):
+    while True:
+        try:
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cur:
+                    await cur.execute("select 1;")
+                    async for row in cur:
+                        print(row)
+        except Exception as e:
+            await asyncio.sleep(3)
+            print(12123214234)
+            pool = await build_db_pool(loop)
+
+async def main(loop):
+    pool = await build_db_pool(loop)
+    asyncio.ensure_future(write_to_db(pool, loop))
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main(loop))
+    asyncio.ensure_future(main(loop))
+    loop.run_forever()
--- elk/brent_oil/brent_from_sina/data_from_sina.py
+++ elk/brent_oil/brent_from_sina/data_from_sina.py
@@ -23,7 +23,7 @@ async def fetch(session, url):
                     data = await resp.text()
                     return data
         except Exception as e:
-            _logger.info('Get url error: {}'.format(e))
+            _logger.error('Get url error: {}'.format(e))
 
 def hanlder_data(data):
     _logger.debug('Origin data: {}'.format(data))
@@ -54,26 +54,50 @@ def hanlder_data(data):
         })
     return res
 
-async def write_to_db(pool, q):
+async def write_to_db(pool, q, loop):
     while True:
-        async with pool.acquire() as conn:
-            async with conn.cursor() as cur:
-                if q.empty():
-                    await asyncio.sleep(0.5)
-                    continue
-                value = await q.get()
-                fields = value.keys()
-                fields = ','.join(fields)
-                vals = value.values()
-                sql = "insert into {} ({}) values {} RETURNING id".format(config['db_table'], fields, tuple(vals))
-                await cur.execute(sql)
+        _logger.debug("Queue's size: {}.".format(q.qsize()))
+        if q.empty():
+            await asyncio.sleep(0.5)
+            continue
+        while pool is None:
+            await asyncio.sleep(1)
+            _logger.warning('Reconnect to the database')
+            pool = await build_db_pool(loop)
+        value = await q.get()
+        _logger.info('Get data from queue successfully.')
+        fields = value.keys()
+        fields = ','.join(fields)
+        vals = value.values()
+        try:
+            async with pool.acquire() as conn:
+                async with conn.cursor() as cur:
+                    sql = "insert into {} ({}) values {} RETURNING id".format(config['db_table'], fields, tuple(vals))
+                    await cur.execute(sql)
+                    async for row in cur:
+                        _logger.info('Create data {} successfully .'.format(row))
+        except Exception as e:
+            _logger.error('Insert data into database unsuccessfully: {}'.format(e))
+            await asyncio.sleep(1)
+            pool = await build_db_pool(loop)
+
+async def build_db_pool(loop):
+    try:
+        dns = 'dbname={} user={} password={} host={} port={}'.format(config['db_name'], config['db_user'],
+                                                                     config['db_password'], config['db_host'],
+                                                                     config['db_port'])
+        pool = await aiopg.create_pool(dns, loop=loop)
+        _logger.info('Successful connect to database {}.'.format(config['db_name']))
+    except Exception as e:
+        _logger.error('Unable to connect to the database: {}'.format(e))
+        pool = None
+    return pool
 
 async def main(loop):
     config.parse_config()
     _logger.info('Script Start..')
-    dns = 'dbname={} user={} password={} host={} port={}'.format(config['db_name'], config['db_user'], config['db_password'], config['db_host'], config['db_port'])
-    pool = await aiopg.create_pool(dns, loop=loop)
-    asyncio.ensure_future(write_to_db(pool, q))
+    pool = await build_db_pool(loop)
+    asyncio.ensure_future(write_to_db(pool, q, loop))
     global check
     async with aiohttp.ClientSession() as session:
         while True:
@@ -82,10 +106,12 @@ async def main(loop):
             _logger.debug('Processed data: {}'.format(res))
             await asyncio.sleep(0.5)
             if check and check == res.get('change_time'):
+                _logger.debug('Skip duplicate data, check time is {}'.format(check))
                 continue
             else:
                 check = res.get('change_time')
                 await q.put(res)
+                _logger.debug('Put data to Queue: {}'.format(res))
 
 #
 # if __name__ == '__main__':
