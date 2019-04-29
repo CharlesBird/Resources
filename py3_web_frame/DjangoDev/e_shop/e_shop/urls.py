d65f3eacbc2f60fe958224fc53b5d24c1023ec34
CB <1016784928@qq.com>
2019-04-29 14:57:40 +0800

    django xadmin后台管理，数据导入

--- py3_web_frame/DjangoDev/e_shop/e_shop/urls.py
+++ py3_web_frame/DjangoDev/e_shop/e_shop/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import xadmin
+from django.views.static import serve
+from e_shop.settings import MEDIA_ROOT
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
+    path('ueditor/', include('DjangoUeditor.urls')),
+    # 文件
+    path('media/<path:path>', serve, {'document_root': MEDIA_ROOT})
 ]
