8aae30d53ef572e3c1b67b79184869c66239641e
CB <1016784928@qq.com>
2019-04-30 15:23:58 +0800

    测试webservice API

--- odoo/runtong/test_webservice.py
+++ odoo/runtong/test_webservice.py
@@ -1,38 +1,16 @@
 import unittest
+#
 
-import suds
 from suds.client import Client
+from suds.xsd.doctor import Import, ImportDoctor
 
-class TestServiceDefinition(unittest.TestCase):
-    def setUp(self):
-        self.client = Client('http://www.thomas-bayer.com/axis2/services/BLZService?wsdl')
 
-    def test_service_representation(self):
-        string_rep = str(self.client)
-        ver = suds.__version__
-        build = suds.__build__.split()[1]
-        expected = """
-Suds ( https://github.com/cackharot/suds-py3 )  version: %s IN  build: %s
-Service ( BLZService ) tns="http://thomas-bayer.com/blz/"
-   Prefixes (1)
-      ns0 = "http://thomas-bayer.com/blz/"
-   Ports (2):
-      (BLZServiceSOAP11port_http)
-         Methods (1):
-            getBank(xs:string blz, )
-         Types (3):
-            detailsType
-            getBankResponseType
-            getBankType
-      (BLZServiceSOAP12port_http)
-         Methods (1):
-            getBank(xs:string blz, )
-         Types (3):
-            detailsType
-            getBankResponseType
-            getBankType
---------------------------------------------------------------------------------""" % (ver, build)
-        self.assertEqual(string_rep, expected)
 
 if __name__ == '__main__':
-    unittest.main()
+    url = 'http://ws.webxml.com.cn/WebServices/WeatherWS.asmx?wsdl'
+    # url = 'http://track.tcs.com.pk/trackingaccount/track.asmx?WSDL'
+    imp = Import('http://www.w3.org/2001/XMLSchema', location='https://www.w3.org/2001/XMLSchema.dtd')
+    imp.filter.add('http://WebXml.com.cn/')
+    doctor = ImportDoctor(imp)
+    client = Client(url, doctor=doctor)
+    print(client)
