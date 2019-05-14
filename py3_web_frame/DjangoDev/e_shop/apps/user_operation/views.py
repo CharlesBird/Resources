42c10ad271229b6835fb22009eb0e6c85ae15a82
CB <1016784928@qq.com>
2019-05-14 09:28:21 +0800

    eshop 收藏权限设置

--- py3_web_frame/DjangoDev/e_shop/apps/user_operation/views.py
+++ py3_web_frame/DjangoDev/e_shop/apps/user_operation/views.py
@@ -1,4 +1,8 @@
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.authentication import SessionAuthentication
+from utils.permissions import IsOwnerOrReadOnly
 from .serializers import UserFavSerializer
 from .models import UserFav
 
@@ -7,5 +11,10 @@ class UserFavViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.Dest
     """
     用户收藏
     """
-    queryset = UserFav.objects.all().order_by("id")
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
+    authentication_classes = (JSONWebTokenAuthentication, SessionAuthentication)
     serializer_class = UserFavSerializer
+
+    def get_queryset(self):
+        # 只能查看当前登录用户的收藏，不会获取所有用户的收藏
+        return UserFav.objects.filter(user=self.request.user)
