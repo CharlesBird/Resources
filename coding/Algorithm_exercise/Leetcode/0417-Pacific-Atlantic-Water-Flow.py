0113f17c07722e9e5c2cf56dec46a4bfe6a005a5
CB <1016784928@qq.com>
2019-08-01 18:05:03 +0800

    leetcode

--- coding/Algorithm_exercise/Leetcode/0417-Pacific-Atlantic-Water-Flow.py
+++ coding/Algorithm_exercise/Leetcode/0417-Pacific-Atlantic-Water-Flow.py
@@ -28,7 +28,42 @@ Return:
 
 [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (positions with parentheses in above matrix).
 """
-# from typing import List
-#
-# class Solution:
-#     def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
+from typing import List
+
+class Solution:
+    def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
+        d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        def dfs(matrix, visited, x, y):
+            visited[x][y] = True
+            for k in range(len(d)):
+                newX = x + d[k][0]
+                newY = y + d[k][1]
+                if newX >= 0 and newX < len(matrix) and newY >= 0 and newY < len(matrix[0]) and \
+                        not visited[newX][newY] and matrix[newX][newY] >= matrix[x][y]:
+                    dfs(matrix, visited, newX, newY)
+
+        res = []
+        if not matrix:
+            return res
+        m, n = len(matrix), len(matrix[0])
+        pacific = [[False for _ in range(n)] for _ in range(m)]
+        atlantic = [[False for _ in range(n)] for _ in range(m)]
+
+        for i in range(m):
+            dfs(matrix, pacific, i, 0)
+            dfs(matrix, atlantic, i, m-1)
+
+        for j in range(n):
+            dfs(matrix, pacific, 0, j)
+            dfs(matrix, atlantic, n-1, j)
+
+        for i in range(m):
+            for j in range(n):
+                if pacific[i][j] and atlantic[i][j]:
+                    res.append([i, j])
+        return res
+
+
+Solution().pacificAtlantic([[1,1],[1,1],[1,1]])
+
