ec98633cc1c1ac5c4568a8d9ff477ea185207472
CB <1016784928@qq.com>
2019-09-18 18:20:44 +0800

    rt

--- odoo/runtong/test_ax_journal_all_data_api.py
+++ odoo/runtong/test_ax_journal_all_data_api.py
@@ -3,9 +3,11 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "http://172.70.0.91:8099/api/AXBarCode/InventTransferJournal/1000-64984"
+url = "http://172.70.0.91:8099/api/AXBarCode/InventTransferJournal/1000-32467"
 
-res = requests.get(url, auth=HTTPBasicAuth('shruntong\Barcode', 'B.rms123'), headers={"Content-Type": "text/plain"})
+data = {"startingPosition": 1, "numberOfRecordsToFetch": 100}
+
+res = requests.get(url, auth=HTTPBasicAuth('shruntong\Barcode', 'B.rms123'), data=data, headers={"Content-Type": "text/plain"})
 print(res.status_code)
 print(res.text)
 json_res = json.loads(res.text)
